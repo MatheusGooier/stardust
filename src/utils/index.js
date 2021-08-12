a4d180045a1947a6df4f84740a122d093b06a762
MatheusGooier <a.matheussouza@gmail.com>
2021-08-12 08:57:39 -0300

    logs on login tests

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,20 @@
 const TOKEN_KEY = "jwt";
 
 export const login = () => {
+  console.log("localStorage.getItem: true");
   localStorage.setItem(TOKEN_KEY, "TestLogin");
 };
 
 export const logout = () => {
+  console.log("localStorage.getItem: false");
   localStorage.removeItem(TOKEN_KEY);
 };
 
 export const isLogin = () => {
   if (localStorage.getItem(TOKEN_KEY)) {
-    console.log("entrou");
+    console.log("localStorage.getItem: true");
     return true;
   }
-
+  console.log("localStorage.getItem: false");
   return false;
 };
